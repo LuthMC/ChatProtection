854650b682d8afae108c0863dfe42ec8a0d08d52
LuthMC <luthfi366822@gmail.com>
2024-08-13 19:19:54 +0700

    PUSH

--- src/Luthfi/ChatProtection/Main.php
+++ src/Luthfi/ChatProtection/Main.php
@@ -20,26 +20,42 @@ class Main extends PluginBase implements Listener {
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        switch ($command->getName()) {
-            case "lock":
-                if ($sender->hasPermission("chatprotection.lock")) {
-                    $this->eventListener->lockChat();
-                    $this->getServer()->broadcastMessage($this->getMessage("chat_locked_by_admin"));
-                    return true;
-                }
-                $sender->sendMessage($this->getMessage("no_permission"));
-                return false;
+    switch ($command->getName()) {
+        case "lock":
+            if ($sender->hasPermission("chatprotection.lock")) {
+                $this->eventListener->lockChat();
+                $this->getServer()->broadcastMessage($this->getMessage("chat_locked_by_admin"));
+                return true;
+            }
+            $sender->sendMessage($this->getMessage("no_permission"));
+            return false;
 
-            case "unlock":
-                if ($sender->hasPermission("chatprotection.unlock")) {
-                    $this->eventListener->unlockChat();
-                    $this->getServer()->broadcastMessage($this->getMessage("chat_unlocked_by_admin"));
-                    return true;
-                }
-                $sender->sendMessage($this->getMessage("no_permission"));
-                return false;
+        case "unlock":
+            if ($sender->hasPermission("chatprotection.unlock")) {
+                $this->eventListener->unlockChat();
+                $this->getServer()->broadcastMessage($this->getMessage("chat_unlocked_by_admin"));
+                return true;
+            }
+            $sender->sendMessage($this->getMessage("no_permission"));
+            return false;
+
+        case "clearchat":
+            if ($sender->hasPermission("chatprotection.clearchat")) {
+                $this->clearChat();
+                $sender->sendMessage($this->getMessage("chat_cleared"));
+                return true;
+            }
+            $sender->sendMessage($this->getMessage("no_permission"));
+            return false;
         }
-        return false;
+    return false;
+    }
+
+    private function clearChat(): void {
+    $clearMessage = str_repeat("\n", 100);
+    foreach ($this->getServer()->getOnlinePlayers() as $player) {
+        $player->sendMessage($clearMessage);
+        }
     }
 
     public function onDisable(): void {
